438f5c3cb6c1f4b87d9b3b9b7d319a346f262d6e
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-03 10:49:26 -0500

    Set up basic Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom'
 import './Layout.css'
-import AboutPage from '../AboutPage'
-import HomePage from '../HomePage'
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <div className="primary-layout">
       <div className="navbar-container">
-          <ul className='navbar'>
-            <li>
-              <Link to="/" className='nav-link'>Home</Link>
-            </li>
-            <li>
-              <Link to="/about" className='nav-link'>About</Link>
-            </li>
-          </ul>
-
-          <Switch>
-            <Route path="/about">
-              <AboutPage />
-            </Route>
-            <Route path="/">
-              <HomePage />
-            </Route>
-          </Switch>
+        {children}
         </div>
     </div>
   )
