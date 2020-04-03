194733df7c48db055c1ee73b8e8a086a803372dc
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-03 10:49:58 -0500

    Set up main App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from "react";
-import {
-  BrowserRouter, Link, Switch, Route
-} from "react-router-dom";
+import Layout from './components/Layout'
+import ChoiceButton from './components/ChoiceButton'
+import QAMap from './QAMap'
 
-import Layout from './components/Layout/Layout'
-import HomePage from './components/HomePage/HomePage'
-import AboutPage from './components/AboutPage/'
-const App = () => (
-  <BrowserRouter>
-    <Layout/>
-  </BrowserRouter>
-)
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentQuestion: 'Are you interested in donating, or do you need funding?',
+      currentChoices: ['Looking for funding', 'Looking to donate']
+    }
+  }
+
+  handleButtonClick = (nextQ) => {
+    this.setState({
+      currentQuestion: QAMap.get(nextQ),
+      currentChoices: QAMap.get(this.state.currentQuestion)
+    })
+  }
+
+  render() {
+    const { currentQuestion, currentChoices } = this.state;
+    return (
+      <div className='App'>
+        <Layout>
+          <div>{currentQuestion}</div>
+          {currentChoices.map((choice) => {
+            return (
+              <ChoiceButton text={choice} handleButtonClick={this.handleButtonClick}/>
+            )
+          })}
+        </Layout>
+      </div>
+    )
+  }
+}
 export default App
