cf854af52a7553fc80c0e3a36cee42b9ff991a01
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-03 22:12:40 -0500

    Add initial questions/choices to Map

--- src/QAMap.js
+++ src/QAMap.js
@@ -1,11 +1,17 @@
 // Each potential answer is mapped to a particular question and vice versa
 
 const QAMap = new Map([
-  ['Are you interested in donating, or do you need funding?', ['Looking for funds', 'Looking to donate']],
+  ['Do you need funding, or are you looking to donate?', ['Looking for funds', 'Looking to donate']],
   ['Looking to donate', 'Is tax-exemption important to you?'],
   ['Is tax-exemption important to you?', ['Yes', 'No']],
+  ['Yes', 'How much money are you trying to donate?'],
+  ['No', 'How much money are you trying to donate?'],
+  ['How much money are you trying to donate?', ['Less than $500', 'Between $500 and $10,000', '$10,000 or more']],
   ['Looking for funds', 'Are you non-profit or for-profit?'],
-  ['How much money are you trying to donate?', ['Less than $500', 'Between $500 and $10,000', '$10,000 or more']]
+  ['Are you non-profit or for-profit?', ['Non-profit', 'For-profit']],
+  ['Less than $500', []],
+  ['Between $500 and $10,000', []],
+  ['$10,000 or more', []]
 ]);
 
 export default QAMap;
