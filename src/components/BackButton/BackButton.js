a49185698a1e2881385e735b7d4a19bdb726d3ef
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-13 09:33:46 -0500

    Conditionally render back button

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './BackButton.css'
 
-const BackButton = ({ handleBackArrowClick }) => {
-  return (
-    <button className='back-arrow' onClick={handleBackArrowClick}>&larr; <div>Back</div></button>
-  )
+const BackButton = ({ handleBackArrowClick, currentQuestion }) => {
+  if (currentQuestion !== 'Do you need funding, or are you looking to donate?') {
+    return (
+      <button className='back-arrow' onClick={handleBackArrowClick}>&larr; <div>Back</div></button>
+    )
+  } else {
+    return null;
+  }
 }
 
 export default BackButton
