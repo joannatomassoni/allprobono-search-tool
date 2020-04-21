59f878455d9860f6e57e8d559d374900fa5df4cd
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-21 12:25:26 -0500

    Flush out questions/answers, add test data for results

--- src/QAMap.js
+++ src/QAMap.js
@@ -7,14 +7,28 @@ const QAMap = new Map([
   ['Yes', 'How much money are you trying to donate?'],
   ['No', 'How much money are you trying to donate?'],
   ['How much money are you trying to donate?', ['Less than $500', 'Between $500 and $10,000', '$10,000 or more']],
-  ['Looking for funds', 'Are you non-profit or for-profit?'],
-  ['Less than $500', []],
-  ['Between $500 and $10,000', []],
-  ['$10,000 or more', []],
-  ['Are you non-profit or for-profit?', ['Non-profit', 'For-profit']],
-  ['Non-profit', 'Are you looking for grants or donations?'],
-  ['For-profit', 'Are you looking for grants or donations?'],
-  ['Are you looking for grants or donations?', ['Grants', 'Donations']],
+
+  ['Looking for funds', 'Grants or donations?'],
+  ['Grants or donations?', ['Looking for grants', 'Looking for donations']],
+  ['Looking for grants', 'What type of organization do you represent?'],
+  ['What type of organization do you represent?', ['Non-profit', 'For-profit', 'Academic']],
+  ['Looking for donations', 'How much?'],
+  ['How much?', ['Less than $10,000', 'More than $100,000', 'Somewhere in between']],
+  
+
+  ['Non-profit', 'Where are you located?'],
+  ['For-profit', 'Where are you located?'],
+  ['Academic', 'Where are you located?'],
+  ['Less than $10,000', 'Where are you located?'],
+  ['More than $100,000', 'Where are you located?'],
+  ['Somewhere in between', 'Where are you located?'],
+
+
+  ['Less than $500', 'Where are you located?'],
+  ['Between $500 and $10,000', 'Where are you located?'],
+  ['$10,000 or more', 'Where are you located?'],
+
+  ['Where are you located?', ['US', 'EU']]
 ]);
 
 export default QAMap;
