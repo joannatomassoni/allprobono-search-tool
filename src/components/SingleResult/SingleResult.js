9ddb4139f3f9d684062ab1ad879b0e743a5d09f6
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-21 12:26:47 -0500

    Style results

--- src/components/SingleResult/SingleResult.js
+++ src/components/SingleResult/SingleResult.js
@@ -2,8 +2,16 @@ import React from 'react'
 import './SingleResult.css'
 
 const SingleResult = ({ result }) => {
+  let rowClass;
+  if (result.id % 2 === 0) {
+    rowClass = 'even'
+  } else {
+    rowClass = 'odd'
+  }
+
+  rowClass = rowClass + ' result-container'
   return (
-    <div className='result-container'>
+    <div className={rowClass}>
       <div className='name result-item'><a href={result.url} target='_blank' rel='noopener noreferrer'>{result.name}</a></div>
       <div className='desc result-item'>{result.description}</div>
     </div>
