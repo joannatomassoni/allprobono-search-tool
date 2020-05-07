4f15573c0ce7ad1f540b0f3417b41476cffe5008
Joanna Tomassoni <jctomassoni@gmail.com>
2020-05-07 18:48:45 -0500

    Modify popup modal

--- src/components/ReactModal/ReactModal.js
+++ src/components/ReactModal/ReactModal.js
@@ -36,9 +36,9 @@ const ReactModal = () => {
                 </button>
                 <div className='modal-text'>
                   <p>
-                    To contribute to our ongoing list of resources, you can <a href='https://www.github.com/joannatomassoni/allprobono-search-tool' el='nofollow noreferrer' target='__blank' title='Head to our Github repo'>make a pull request</a> in our Github repo. 
+                    <b>DEVELOPERS</b>: <a href='https://www.github.com/joannatomassoni/allprobono-search-tool' el='nofollow noreferrer' target='__blank' title='Head to our Github repo'>make a pull request</a> in our Github repo. 
                     <br/><br/> 
-                    Or just <a href='mailto:apb2020@gmail.com?subject=Request to add to database' rel='nofollow noreferrer' target='__blank' title='Email us'>email us</a> with the <em>name</em>, <em>website</em>, a <em>description</em> of your organization, and anything else you'd like us to know.
+                    <b>EVERYONE ELSE</b>: <a href='mailto:apb2020@gmail.com?subject=Request to add to database' rel='nofollow noreferrer' target='__blank' title='Email us'>email us</a> with the <em>name</em>, <em>website</em>, a <em>description</em> of your organization, and anything else you'd like us to know.
                     </p>
               </div>
             </div>
