74f839c2e8ecddabe700be212733df5cb46b6ec7
Joanna Tomassoni <jctomassoni@gmail.com>
2020-05-07 11:00:33 -0500

    Reconfigure layout of main content

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -3,16 +3,26 @@ import './Results.css'
 import SingleResult from '../SingleResult';
 
 const Results = ({ results }) => {
-  return (
-    <div className='results-container'>
+  if (results.length) {
+    return (
+      <div className='results-container'>
+        <h2>Results</h2>
+        {results.map((result) => {
+          return (
+            <SingleResult result={result}/>
+          )
+        })}
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className='results-container'>
       <h2>Results</h2>
-      {results.map((result) => {
-        return (
-          <SingleResult result={result}/>
-        )
-      })}
+      <p>Sorry, there don't appear to have any organizations that meet your needs at this time.</p>
     </div>
-  )
+    )
+  }
 }
 
 export default Results
